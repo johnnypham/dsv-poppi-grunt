fa42513f572af180ed07963ba634652c111faabd
Vienpn <vienpn@designveloper.com>
2016-04-21 15:46:00 +0700

    update js

---
+++ jade/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  $("#infoUser-tabs").tabs();
+  $("#tabs-settings").tabs();
+  $("#tabs-conversation-profile").tabs();
+  $(".show-hide-nav").click(function(event) {
+    event.preventDefault();
+    $(this).toggleClass("navActive");
+    $('.sidebar-left').toggleClass("navActive");
+  });
+  $('#accordion-conversation-profile').accordion();
+  $("#infoUser-Show").click(function(event) {
+    event.preventDefault();
+    $('#toggleUser').toggleClass("fadeInDown");
+  });
+  $("#toggleEmailUser-Show").click(function(event) {
+    event.preventDefault();
+    $('#toggleEmailUser').toggleClass("fadeInDown");
+  });
+
+
+  $('.toggle-Details-conversation').on('click', function() {
+    $(this).closest('.tabs-conversations-content-user').addClass('show-conversations')
+  });
+  $('.close-conversation').on('click', function() {
+    $(this).closest('.tabs-conversations-content-user').removeClass('show-conversations')
+  })
+
+  $('.content-table').on('click', function() {
+    $(this).toggleClass('show-toggle-invoices')
+  });
+  $('#cp7').colorpicker({
+    color: '#ffaa00',
+    container: true,
+    inline: true
+  });
+});
